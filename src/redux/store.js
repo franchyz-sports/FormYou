6a0b91eaccecd0702b0e9afb8b2636da1f41de15
PabloSKZ <pablo.eskenazi@gmail.com>
2020-06-12 07:12:03 +0200

    redux bug fixed

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import thunkMiddleware from 'redux-thunk';
 
 const store = createStore(
   rootReducer,
-  compose(applyMiddleware(thunkMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  compose(applyMiddleware(thunkMiddleware))
 )
 
 store.subscribe(() => console.log(store.getState()));
